5fb400a6355615cbabac5ff60d374566ba88dead
Edgar Rudakov <edgar.rudakov@mail.ru>
2014-12-09 18:53:37 +0300

    Monstr

--- monstr.cpp
+++ monstr.cpp
@@ -3,6 +3,12 @@
 void Monstr::Set_agrrange(int n){agrrange=n;}
 int Monstr::Get_agrrange(){return agrrange;}
 
+void Monstr::Change_heroin(){
+    heroin++;
+    heroin%=2;
+}
+
+int Monstr::Get_heroin(){return heroin;}
 
 
 
