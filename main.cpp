02f86e4c97f3bd6ecae374d52fca6c9da617468f
DarkEric <darkericos@gmail.com>
2014-12-10 21:27:47 +0200

    Откат

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
     #include "Menu.h"
-
+    #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
     int main()
     {
         setlocale(LC_ALL, "ru");
-        system("mode con cols=60 lines=30");
-        MENU();
+//        system("mode con cols=60 lines=30");
+        std::cout<<"╔";
+ //       MENU();
         return 0;
     }
