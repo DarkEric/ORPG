2465f29f1c4955a81d0415d0bddeb4382874f437
DarkEric <darkericos@gmail.com>
2014-12-10 21:28:04 +0200

    Revert "Откат"
    This reverts commit 02f86e4c97f3bd6ecae374d52fca6c9da617468f.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
     #include "Menu.h"
-    #include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
+
     int main()
     {
         setlocale(LC_ALL, "ru");
-//        system("mode con cols=60 lines=30");
-        std::cout<<"╔";
- //       MENU();
+        system("mode con cols=60 lines=30");
+        MENU();
         return 0;
     }
