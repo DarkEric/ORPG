2bee8f026a836ee646f905c804bb0867b6cd0df1
Edgar Rudakov <edgar.rudakov@mail.ru>
2014-12-13 14:28:03 +0300

    Окно боя без скиллов и атак, более менее готово

--- FightWindow.cpp
+++ FightWindow.cpp
@@ -1,39 +1,69 @@
 #include "FightWindow.h"
 #include <string.h>
-#include <windows.h>
 
-using std::string;
+using namespace std;
+
+void PrintPictures(){
+    system("cls");
+    for (int i=1;i<=31;i++)printf("*");
+    printf("\n");
+    for (int i=2;i<=39;i++){
+        printf("*");
+        for (int j=2;j<=14;j++) printf("0");
+        printf("*");
+        for (int j=16;j<=30;j++) printf("0");
+        printf("*\n");
+    }
+    for (int i=1;i<=31;i++)printf("*");
+    return;
+}
+
+void IntToChar(int a,char* mas,int p){
+    int b=a;
+    int l=0;
+    while (b>0){
+        l++;
+        b/=10;
+    }
+    while (a>0){
+        mas[l+p-1]=a%10+48;
+        a/=10;
+    }
+}
+
+void FormStatsMas(Critter* character, Critter* mob, char** mas){
+
+    int hp_char=character->Get_HP(),hp_mob=mob->Get_HP();
+    IntToChar(hp_char,mas[2],1); //Вписываем
+    IntToChar(hp_mob,mas[2],16);
+
+}
+
+void PrintStats(char** mas){
+    for (int i=1;i<=3;i++){
+        for (int j=1;j<=31;j++) printf("%c",mas[i][j]);
+        printf("\n");
+    }
+}
 
 int Fight(Critter* character,Critter* mob)
 {
-    setlocale(LC_ALL, "ru");
     system("mode con cols=31 lines=80");
+    char** mas=new char*[4];
+    for (int i=0;i<4;i++){
+        mas[i]=new char[32];
+    }
     string n1=character->Get_name();
     string n2=mob->Get_name();
-//    int ln1=strlen(n1); //ПШЕ
-//    string hp_char=character->Get_HP(); // HP В INT
-//    string hp_mob=mob->Get_HP();
-//    HANDLE hw = GetStdHandle(STD_OUTPUT_HANDLE);
+    int ln1=n1.size();
+    int ln2=n2.size();
+    for (int i=1;i<=ln1;i++) mas[1][i]=n1[i-1];
+    for (int i=16;i<=16+ln2;i++) mas[1][i]=n2[i-1];
     while ((character->Status_Life())||(mob->Status_Life())){
-        system("cls");
-        for (int i=1;i<=31;i++)printf("*");
-        printf("\n");
-        for (int i=2;i<=39;i++){
-            printf("*");
-            for (int j=2;j<=14;j++) printf("0");
-            printf("*");
-            for (int j=16;j<=30;j++) printf("0");
-            printf("*\n");
-        }
-        for (int i=1;i<=31;i++)printf("*");
+        PrintPictures();
         printf("\n\n");
-        
- //       printf("%s",n1);
- //       for (int i=ln1+1;i<=16;i++)printf(" ");
-  //      printf("%s\n",n2);
-        
-        //SetConsoleCursorPosition(hw,)
-        
+        FormStatsMas(character,mob,mas);
+        PrintStats(mas);
     }
         
     return 0;
