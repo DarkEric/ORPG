4537507be835f5fabc2f42b975b19373188dcd5b
DarkEric <darkericos@gmail.com>
2014-12-13 18:25:10 +0200

    Temp

--- FightWindow.cpp
+++ FightWindow.cpp
@@ -32,7 +32,7 @@ void IntToChar(int a,char* mas,int p){
     }
 }
 
-void FormStatsMas(Critter* character, Critter* mob, char** mas){
+/*void FormStatsMas(Critter* character, Critter* mob, char** mas){
 
     int hp_char=character->Get_HP(),hp_mob=mob->Get_HP();
     int energy_char=character->Get_energy();
@@ -42,36 +42,41 @@ void FormStatsMas(Critter* character, Critter* mob, char** mas){
     IntToChar(energy_char,mas[3],1);
     IntToChar(energy_mob,mas[3],32);
 
-}
+}*/
 
-void PrintStats(char** mas){
+/*void PrintStats(char** mas){
     for (int i=1;i<=3;i++){
         for (int j=1;j<=61;j++) printf("%c",mas[i][j]);
         printf("\n");
     }
-}
+
+}*/
 
 int Fight(Critter* character,Critter* mob)
 {
-    system("mode con cols=61 lines=60");
+    system("mode con cols=91 lines=50");
     int prior=1;
-    char** mas=new char*[4];
+   /* char** mas=new char*[4];
     for (int i=0;i<4;i++){
         mas[i]=new char[62];
-    }
+    }*/
     string n1=character->Get_name();
     string n2=mob->Get_name();
-    int ln1=n1.size();
-    int ln2=n2.size();
-    for (int i=1;i<=ln1;i++) mas[1][i]=n1[i-1];
-    for (int i=32;i<=32+ln2;i++) mas[1][i]=n2[i-1];
+   // int ln1=n1.size();
+  //  int ln2=n2.size();
+  //  for (int i=1;i<=ln1;i++) mas[1][i]=n1[i-1];
+  //  for (int i=32;i<=32+ln2;i++) mas[1][i]=n2[i-1];
     while ((character->Status_Life())&&(mob->Status_Life())){
-        PrintPictures();
+       //PrintPictures();
         printf("\n\n");
-        FormStatsMas(character,mob,mas);
-        PrintStats(mas);
-        if (prior){
+        //FormStatsMas(character,mob,mas);
+        cout << n1 << "                  " <<n2<<endl;
+        cout << character->Get_HP() << "            " << mob->Get_HP()<< endl;
+      //  cout << character->Get_energy()<<"             "<<mob->Get_energy()<<endl;
+       // PrintStats(mas);
+        if (prior==1){
             character->Attack(mob);
+            cout<< "Герой атаковал" << endl;
             prior+=1; // Смена приоритета
             prior%=2; //
             Sleep(1);
@@ -82,15 +87,16 @@ int Fight(Critter* character,Critter* mob)
                 break;
             }
         }
-        if (prior){
+        if (prior==0){
             mob->Attack(character);
+            cout<< "Mob атаковал" << endl;
             Sleep(1);
             prior+=1;
             prior%=2;
             if (character->Get_HP()<0) {
                 character->Set_Life(false);
                 system("cls");
-                printf("Вы погибли...Игра окончена");
+                cout << "Вы погибли...Игра окончена";
                 break;
             }
         }
