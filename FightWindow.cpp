a9e489e018edf54ecc359f60e5d147a44d30a636
Edgar Rudakov <edgar.rudakov@mail.ru>
2014-12-13 17:58:21 +0300

    Попытка №2

--- FightWindow.cpp
+++ FightWindow.cpp
@@ -38,15 +38,15 @@ void FormStatsMas(Critter* character, Critter* mob, char** mas){
     int energy_char=character->Get_energy();
     int energy_mob=mob->Get_energy();
     IntToChar(hp_char,mas[2],1); //Вписываем
-    IntToChar(hp_mob,mas[2],16);
+    IntToChar(hp_mob,mas[2],32);
     IntToChar(energy_char,mas[3],1);
-    IntToChar(energy_mob,mas[3],16);
+    IntToChar(energy_mob,mas[3],32);
 
 }
 
 void PrintStats(char** mas){
     for (int i=1;i<=3;i++){
-        for (int j=1;j<=31;j++) printf("%c",mas[i][j]);
+        for (int j=1;j<=61;j++) printf("%c",mas[i][j]);
         printf("\n");
     }
 }
@@ -57,14 +57,14 @@ int Fight(Critter* character,Critter* mob)
     int prior=1;
     char** mas=new char*[4];
     for (int i=0;i<4;i++){
-        mas[i]=new char[32];
+        mas[i]=new char[62];
     }
     string n1=character->Get_name();
     string n2=mob->Get_name();
     int ln1=n1.size();
     int ln2=n2.size();
     for (int i=1;i<=ln1;i++) mas[1][i]=n1[i-1];
-    for (int i=16;i<=16+ln2;i++) mas[1][i]=n2[i-1];
+    for (int i=32;i<=32+ln2;i++) mas[1][i]=n2[i-1];
     while ((character->Status_Life())&&(mob->Status_Life())){
         PrintPictures();
         printf("\n\n");
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-
+#include "berserk.h"
+#include "troll.h"
+#include "FightWindow.h"
 
     int main()
     {
+        Berserk Vasya("Vasya");
+        Troll Kolya(1,1);
+        Fight(&Vasya,&Kolya);
         return 0;
     }
