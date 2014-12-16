1400c5766599d9096d70290128feba1c9bbcdba1
Edgar Rudakov <edgar.rudakov@mail.ru>
2014-12-16 19:19:32 +0300

    Массив стоимостей скиллов+дракон

--- dragon.h
+++ dragon.h
@@ -1,22 +1,26 @@
 #ifndef DRAGON_H
 #define DRAGON_H
-
+#include "monstr.h"
 class Dragon : public Monstr
 {
 public:
-    Dragon():Monstr("Dragon",ID_TYPE_TROLL,TROLL_START_HP,TROLL_PATTACK,TROLL_PDEFENSE,1,1,TROLL_LEVEL,TROLL_START_ENERGY,TROLL_MDEFENSE){
-        this->Set_exp_reward(TROLL_EXPERIENCE);
+    Dragon():Monstr("Dragon",ID_TYPE_DRAGON,DRAGON_START_HP,DRAGON_PATTACK,DRAGON_PDEFENSE,1,1,DRAGON_LEVEL,DRAGON_START_ENERGY,DRAGON_MDEFENSE){
+        this->Set_exp_reward(DRAGON_EXPERIENCE);
         for (int i=1;i<=7;i++) {
             this->Set_name_SP(i,"0");
+            this->Set_energy_coast(i,0);
         }
-//        this->Set_name_SP(1,"Power Strike");
-//        this->Set_name_SP(2,"Tail Strike");
-//        this->Set_name_SP(3,"Flame Breath");
-//        this->Set_name_SP(4,"Flame Breath");
+        this->Set_name_SP(1,"1");
+        this->Set_energy_coast(1,5);
+        this->Set_name_SP(2,"1");
+        this->Set_energy_coast(2,10);
+        this->Set_name_SP(3,"1");
+        this->Set_energy_coast(3,15);
     }
     void Attack(Critter *whom);
     void SP1(Critter *whom);
     void SP2(Critter *whom);
+    void SP3(Critter *whom);
 };
 
 #endif // DRAGON_H
