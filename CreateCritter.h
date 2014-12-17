f6a112f473428772f83c358fa4182330683ec8ad
Eism <eismailzada@gmail.com>
2014-12-17 22:33:26 +0200

    создание controller

--- CreateCritter.h
+++ CreateCritter.h
@@ -3,8 +3,9 @@
 #include "QNPC.h"
 #include "troll.h"
 #include "BUM.h"
+#include "dragon.h"
 QNPC *CreateQuest(int ,QNPC* ,int** );
 BUM *CreateBum(int , BUM * , int** );
 void CreateTroll(int ,Monstr** ,int** );
-
+Dragon *CreateBum(int , Dragon * , int** );
 #endif // CREATECRITTER_H
