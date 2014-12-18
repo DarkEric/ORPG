801c2dff05c5946ac1a7638eff79e95688942f13
DarkEric <darkericos@gmail.com>
2014-12-18 19:18:26 +0200

    sdgf

--- main.cpp
+++ main.cpp
@@ -3,27 +3,31 @@
 //#include "Menu.h"
 #include "Weapon.h"
 #include "Armor.h"
-
+int Kweapon,Karmor;
 Weapon Mweapon[100];
 Armor Marmor[100];
 
 int main()
 {
-    setlocale(LC_ALL, "rus");
+    std::setlocale(LC_ALL,"Russian_Russia.1251");
     std::ifstream in("data/Weapon.bon",std::ios::binary|std::ios::in);
-    int k=-1;
-    in>>k;
-    for(int i=0;i<=k;i++){
+    int Kweapon=-1;
+    in>>Kweapon;
+    for(int i=0;i<=Kweapon;i++){
         in.read((char*)&Mweapon[i],sizeof(Weapon));
     }
     in.close();
     in.open("data/Armor.bon",std::ios::binary|std::ios::in);
-    k=-1;
-    in>>k;
-    for(int i=0;i<=k;i++){
+    Karmor=-1;
+    in>>Karmor;
+    for(int i=0;i<=Karmor;i++){
         in.read((char*)&Marmor[i],sizeof(Armor));
     }
     in.close();
+    std::setlocale(LC_ALL,"rus_rus.866");
+    std::cout<<Mweapon[0].Get_name();
+    std::cout<<Marmor[0].Get_name();
+    std::setlocale(LC_ALL,"Russian_Russia.1251");
 //    system("mode con cols=80 lines=40");
 //    MENU();
 
